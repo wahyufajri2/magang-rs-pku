0d42c3efccbebb1de09cfffa05dab4da490b288e
wahyufajri2 <2011501020@student.unisayogya.ac.id>
2023-05-08 14:40:57 +0700

    Menambahkan file admin

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -42,7 +42,7 @@
         <footer class="py-4 bg-light mt-auto">
             <div class="container-fluid px-4">
                 <div class="d-flex align-items-center justify-content-between small">
-                    <div class="text-muted">Copyright &copy; Your Website 2023</div>
+                    <div class="text-muted">Copyright &copy; RS PKU Gamping <?= date('Y'); ?></div>
                     <div>
                         <a href="#">Privacy Policy</a>
                         &middot;
