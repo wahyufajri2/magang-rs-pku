8f91fa514fc0ec5174a140951ead7bd754dffe9f
wahyufajri2 <2011501020@student.unisayogya.ac.id>
2023-05-10 08:54:22 +0700

    Menambahkan halaman dashboard

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -1,15 +1,16 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Dashboard extends CI_Controller {
+class Dashboard extends CI_Controller
+{
 
 	public function index()
 	{
 		$data['title'] = 'Dashboard';
 
-		$this->load->view('templates/header', $data );
-        $this->load->view('templates/sidebar', $data);
-        $this->load->view('dashboard');
-        $this->load->view('templates/footer');
+		$this->load->view('templates/header', $data);
+		$this->load->view('templates/sidebar', $data);
+		$this->load->view('dashboard', $data);
+		$this->load->view('templates/footer');
 	}
 }
--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,1 +1,41 @@
-HALO HALO
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+
+    <div class="card-deck">
+        <div class="card">
+            <img class="card-img-top" src="<?= base_url('asset/img/profile/') . $user['image']; ?>">
+            <div class="card-body">
+                <h5 class="card-title"><?= $user['name']; ?></h5>
+                <p class="card-text"><?= $user['email']; ?></p>
+            </div>
+            <div class="card-footer">Member since <?= date('d F Y', $user['date_created']); ?></small>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="<?= base_url('asset/img/profile/') . $user['image']; ?>">
+            <div class="card-body">
+                <h5 class="card-title"><?= $user['name']; ?></h5>
+                <p class="card-text"><?= $user['email']; ?></p>
+            </div>
+            <div class="card-footer">Member since <?= date('d F Y', $user['date_created']); ?></small>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="<?= base_url('asset/img/profile/') . $user['image']; ?>">
+            <div class="card-body">
+                <h5 class="card-title"><?= $user['name']; ?></h5>
+                <p class="card-text"><?= $user['email']; ?></p>
+            </div>
+            <div class="card-footer">Member since <?= date('d F Y', $user['date_created']); ?></small>
+            </div>
+        </div>
+    </div>
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
