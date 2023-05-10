d3cff04b63c7b3f03bb7b5b0e03b21abbc93521c
wahyufajri2 <2011501020@student.unisayogya.ac.id>
2023-05-10 10:20:56 +0700

    Memperbarui menu register

--- application/views/auth/registration.php
+++ application/views/auth/registration.php
@@ -6,45 +6,42 @@
                     <div class="col-lg-7">
                         <div class="card shadow-lg border-0 rounded-lg mt-5">
                             <div class="card-header">
-                                <h3 class="text-center font-weight-light my-4">Create Account</h3>
+                                <h3 class="text-center font-weight-light my-4">Create an Account</h3>
                             </div>
                             <div class="card-body">
-                                <form>
+                                <form class="user" method="post" action="<?= base_url('auth/registration'); ?>">
                                     <div class="row mb-3">
-                                        <div class="col-md-6">
+                                        <div class="col-md">
                                             <div class="form-floating mb-3 mb-md-0">
-                                                <input class="form-control" id="inputFirstName" type="text" placeholder="Enter your first name" />
-                                                <label for="inputFirstName">First name</label>
+                                                <input type="text" class="form-control form-control-user" id="name" name="name" placeholder="Full name" value="<?= set_value('name'); ?>">
+                                                <?= form_error('name', '<small class="text-danger pl-3">', '</small>'); ?>
+                                                <label for="name">Full name</label>
                                             </div>
-                                        </div>
-                                        <div class="col-md-6">
-                                            <div class="form-floating">
-                                                <input class="form-control" id="inputLastName" type="text" placeholder="Enter your last name" />
-                                                <label for="inputLastName">Last name</label>
+                                            <div class="form-floating mb-3">
+                                                <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Email Address" value="<?= set_value('email'); ?>">
+                                                <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
+                                                <label for="email">Email address</label>
                                             </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-floating mb-3">
-                                        <input class="form-control" id="inputEmail" type="email" placeholder="name@example.com" />
-                                        <label for="inputEmail">Email address</label>
-                                    </div>
-                                    <div class="row mb-3">
-                                        <div class="col-md-6">
-                                            <div class="form-floating mb-3 mb-md-0">
-                                                <input class="form-control" id="inputPassword" type="password" placeholder="Create a password" />
-                                                <label for="inputPassword">Password</label>
+                                            <div class="row mb-3">
+                                                <div class="col-md-6">
+                                                    <div class="form-floating mb-3 mb-md-0">
+                                                        <input type="password" class="form-control form-control-user" id="password1" name="password1" placeholder="Password">
+                                                        <?= form_error('password1', '<small class="text-danger pl-3">', '</small>'); ?>
+                                                        <label for="password1">Password</label>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-floating mb-3 mb-md-0">
+                                                        <input type="password" class="form-control form-control-user" id="password2" name="password2" placeholder="Repeat Password">
+                                                        <label for="password2">Confirm Password</label>
+                                                    </div>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div class="col-md-6">
-                                            <div class="form-floating mb-3 mb-md-0">
-                                                <input class="form-control" id="inputPasswordConfirm" type="password" placeholder="Confirm password" />
-                                                <label for="inputPasswordConfirm">Confirm Password</label>
+                                            <div class="mt-4 mb-0">
+                                                <button type="submit" class="d-grid" class="btn btn-primary btn-user btn-block">
+                                                    Register Account
+                                                </button>
                                             </div>
-                                        </div>
-                                    </div>
-                                    <div class="mt-4 mb-0">
-                                        <div class="d-grid"><a class="btn btn-primary btn-block" href="login.html">Create Account</a></div>
-                                    </div>
                                 </form>
                             </div>
                             <div class="card-footer text-center py-3">
